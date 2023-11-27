1105277eaa30091b07f459b0cfceed11d3dc7f22
Eliud <58745656+eliudio@users.noreply.github.com>
2023-11-27 22:35:12 +0000

    Refactor

--- lib/tools/bespoke_entities.dart
+++ lib/tools/bespoke_entities.dart
@@ -1,3 +1,3 @@
 // Used by generated classes to have access to entities used within those classes
 export 'package:eliud_core_main/apis/action_api/action_entity.dart';
-export 'package:eliud_core_main/apis_impl/action/goto_page.dart';
+export 'package:eliud_core_main/apis/action_api/actions/goto_page.dart';
